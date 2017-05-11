daed33163d53b522d3db67e3c930f475ae6dedb1
Patrick Johansen <Patrick Johansen>
2017-05-11 14:58:28 +0200

    Made a quick fix for objectives fragment navigation and implemented none-working extension to supportmapfragment

--- kotlin-anko-template/app/src/main/java/dk/cphbusiness/template/MapActivity.kt
+++ kotlin-anko-template/app/src/main/java/dk/cphbusiness/template/MapActivity.kt
@@ -12,9 +12,14 @@ import kotlinx.android.synthetic.main.activity_map.*
 import org.jetbrains.anko.onClick
 
 class MapActivity : FragmentActivity(), OnMapReadyCallback {
+
+    private var mMap: GoogleMap? = null
+
+    var isShowingObj : Boolean = false
+
     val objxFragment = ObjFragment(this)
     val mapFragment = SupportMapFragment()
-    private var mMap: GoogleMap? = null
+    //val mySupMapFragment = MySupportMapFragment(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,37 +28,66 @@ class MapActivity : FragmentActivity(), OnMapReadyCallback {
         supportFragmentManager
                 .beginTransaction()
                 .add(R.id.map_fragment_container, objxFragment)
+                //.add(R.id.map_fragment_container, mySupMapFragment)
                 .add(R.id.map_fragment_container, mapFragment)
                 .hide(objxFragment)
                 .commit()
 
-        objBtn.onClick { showObj() }
-        backBtn.onClick {finish()}
+        objBtn.onClick {
+            if (!isShowingObj) showObj()
+            else hideObj()
+        }
+        endBtn.onClick { finish() }
     }
+
+    fun showMap() {
+        supportFragmentManager
+                .beginTransaction()
+                //.show(mySupMapFragment)
+                .hide(objxFragment)
+                .show(mapFragment)
+                .commit()
+    }
+
+    fun hideMap() {
+        supportFragmentManager
+                .beginTransaction()
+                //.hide(mySupMapFragment)
+                .show(objxFragment)
+                .hide(mapFragment)
+                .commit()
+    }
+
 
     fun hideObj() {
         supportFragmentManager
                 .beginTransaction()
                 .hide(objxFragment)
+                //.show(mySupMapFragment)
                 .show(mapFragment)
                 .commit()
+        isShowingObj = false
     }
 
     fun showObj() {
         supportFragmentManager
                 .beginTransaction()
                 .show(objxFragment)
+                //.hide(mySupMapFragment)
                 .hide(mapFragment)
                 .commit()
+        isShowingObj = true
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
+
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
         mMap!!.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap!!.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
     }
 
 }
