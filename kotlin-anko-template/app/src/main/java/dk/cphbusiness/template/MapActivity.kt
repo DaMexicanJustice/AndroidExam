9533394c03787fc88de9f580ecabe15d2f6d7657
Patrick Johansen <Patrick Johansen>
2017-05-18 12:48:48 +0200

    Added permission handling to access fine location using gps

--- kotlin-anko-template/app/src/main/java/dk/cphbusiness/template/MapActivity.kt
+++ kotlin-anko-template/app/src/main/java/dk/cphbusiness/template/MapActivity.kt
@@ -1,29 +1,42 @@
 package dk.cphbusiness.template
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
 import android.support.v4.app.FragmentActivity
 import android.util.Log
-import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.activity_map.*
 import org.jetbrains.anko.onClick
+import org.jetbrains.anko.toast
 
 
 
 
-class MapActivity : FragmentActivity(), OnMapReadyCallback {
+
+
+
+
+class MapActivity : FragmentActivity(), OnMapReadyCallback, LocationListener, ActivityCompat.OnRequestPermissionsResultCallback {
 
     private val cat = "MapA"
+    private val errorMsg : String = " signal has been lost"
+
+    private val LOCATION_PERMISSION_REQUEST_CODE = 1
 
     private var mMap: GoogleMap? = null
     lateinit private var locationManager : LocationManager
+    private val _minDistance: Float = 2.0f
+    private val _minTime: Long = 2L
+    private var longitude : Double = 0.0
+    private var latitude : Double = 0.0
 
     var isShowingObj : Boolean = false
 
@@ -51,7 +64,40 @@ class MapActivity : FragmentActivity(), OnMapReadyCallback {
 
         mapFragment.getMapAsync(this)
 
-        //locationManager = applicationContext.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        locationManager = applicationContext.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        if ( checkLocationPermission() == false ) {
+            //TODO Dialogue box here explaining why we need permission
+            ActivityCompat.requestPermissions(this, arrayOf<String>(Manifest.permission.ACCESS_FINE_LOCATION), 1)
+        }
+
+        if ( isNetworkEnabled() ) {
+            if ( isGPSEnabled() ) {
+                //TODO write code here
+
+
+            } else {
+                toast("GPS${errorMsg}")
+            }
+
+        } else {
+            toast("Internet${errorMsg}")
+        }
+    }
+
+    fun updateUserLocation() {
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, _minTime, _minDistance, this)
+        var location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+        latitude = location.latitude
+        longitude = location.longitude
+    }
+
+    fun isNetworkEnabled() : Boolean {
+        return locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
+    fun isGPSEnabled() : Boolean {
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
     }
 
     fun showMap(mapFragment : SupportMapFragment) {
@@ -97,10 +143,10 @@ class MapActivity : FragmentActivity(), OnMapReadyCallback {
         Log.d(cat, "MapReady")
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
+        /* Add a marker in Sydney and move the camera. Test case
         val sydney = LatLng(-34.0, 151.0)
         mMap!!.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap!!.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        mMap!!.moveCamera(CameraUpdateFactory.newLatLng(sydney)) */
     }
 
     fun checkNetwork() : Boolean {
@@ -111,4 +157,41 @@ class MapActivity : FragmentActivity(), OnMapReadyCallback {
         return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
     }
 
+    override fun onLocationChanged(p0: Location?) {
+        toast("Am I somewhere else?")
+    }
+
+    override fun onStatusChanged(p0: String?, p1: Int, p2: Bundle?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onProviderEnabled(p0: String?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onProviderDisabled(p0: String?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            1 -> {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return
+            }
+        }// other 'case' lines to check for other
+        // permissions this app might request
+    }
+
+    fun checkLocationPermission(): Boolean {
+        val permission = "android.permission.ACCESS_FINE_LOCATION"
+        val res = this.checkCallingOrSelfPermission(permission)
+        return res == PackageManager.PERMISSION_GRANTED
+    }
 }
